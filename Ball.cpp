8e2691e1313c4f95a18afffdad701d20eb4d657c
Omar Walid <omarwalidhamed@gmail.com>
2022-11-14 19:58:40 +0100

    Created the Ball class

---
+++ Ball.cpp
@@ -0,0 +1,29 @@
+#include "Ball.h"
+
+
+
+
+
+Ball::Ball(const glm::vec2 position, const glm::vec2 velocity = { 0, 0 }) {
+	this->position = position;
+	this->velocity = velocity;
+}
+
+void Ball::setPosition(glm::vec2 newPos) {
+	position = newPos;
+}
+
+glm::vec2 Ball::getVelocity()
+{
+	return velocity;
+}
+
+glm::vec2 Ball::getPosition()
+{
+	return position;
+}
+
+
+void Ball::setVelocity(glm::vec2 newVelocity) {
+	velocity = newVelocity;
+}
--- Ball.h
+++ Ball.h
@@ -1,9 +1,26 @@
 #pragma once
 
+#include "glm/vec2.hpp"
+#include "Color.h"
+
+
+static const Color ballColor = {0.3f, 0.2f, 0.6f};
+static const float ballRadius = 20.0f;
 
 class Ball {
 
+private:
+	glm::vec2 velocity;
+	glm::vec2 position;
 
 
+public:
+	void setVelocity(glm::vec2);
+	void setPosition(glm::vec2);
+	glm::vec2 getVelocity();
+	glm::vec2 getPosition();
+
+	Ball() = delete;
+	Ball(const glm::vec2 position, const glm::vec2 velocity = { 0, 0 });
 
 };
